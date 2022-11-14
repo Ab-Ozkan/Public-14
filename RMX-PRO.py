e61e826399ffca74409fb3f1af0f52a35006b874
Abdullah Ozkan <118141682+Ab-Ozkan@users.noreply.github.com>
2022-11-14 06:06:33 +0600

    Add files via upload

---
+++ RMX-PRO.py
@@ -0,0 +1,24 @@
+import os
+
+ 
+
+ 
+
+os.system('git pull')
+
+ 
+
+ 
+
+os.system('termux-setup-storage')
+
+
+os.system('xdg-open https://facebook.com/groups/770617227293870/')
+ 
+
+
+import PB44
+ 
+
+
+PB44.rmx()
